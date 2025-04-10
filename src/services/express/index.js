53f36d113f45dddf9c957f3048abc52539f07a37
abhiman <94101871+abhi777777@users.noreply.github.com>
2025-04-10 16:58:16 +0530

    added cors

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 function createExpressApp(apiRoot, routes) {
   const app = express();
   app.use(express.json());
   app.use(apiRoot, routes);
+  app.use(cors());
   return app;
 }
 
